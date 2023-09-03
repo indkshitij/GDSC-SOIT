a0a5a77f68249f8261c1f9e5fc61b184806e9d30
Kshitij Singh <138932243+indkshitij@users.noreply.github.com>
2023-09-03 18:49:27 +0530

    GDSC

---
+++ index.js
@@ -0,0 +1,34 @@
+const menuButton = document.getElementById("menu");
+const closeButton = document.getElementById("close");
+const linkContainer = document.querySelector(".link");
+
+function openMenu() {
+    linkContainer.style.visibility = "visible";
+  }
+function closeMenu() {
+  linkContainer.style.visibility = "hidden";
+}
+
+
+
+if (window.innerWidth < 768) {
+    closeMenu();
+  }
+
+menuButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
+
+// Scroll Progress Bar
+const progressBar = document.getElementById("progressBar");
+
+window.onscroll = function () {
+  const totalHeight = document.body.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+  let percentageScroll = (scrollY / (totalHeight - clientHeight)) * 100;
+  if (percentageScroll <= 1) {
+    progressBar.style.width = "0%";
+  } else {
+    progressBar.style.width = percentageScroll + "%";
+
+  }
+};
